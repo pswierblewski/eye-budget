0d83ae8c947e3d814c077b1e2d31eff470947bdc
pswierblewski <p.swierblewski@gmail.com>
2025-06-27 12:57:45 +0200

    ocr

--- src/app.py
+++ src/app.py
@@ -2,18 +2,20 @@ from abc import ABC
 from repositories.receipts_repository import ReceiptsRepository
 from repositories.files_repository import FilesRepository
 from services.preprocessing import PreprocessingService
+from services.ocr import OCRService
 
 class App(ABC):
     def __init__(self):
         self.receipts_repository = ReceiptsRepository()
         self.files_repository = FilesRepository()
         self.preprocessing_service = PreprocessingService()
+        self.ocr_service = OCRService()
     
     def run(self):
-        self.receipts_repository.download_receipts()
-        files = self.files_repository.list_input_files()
-        for file in files:
-            processed_file = self.preprocessing_service.preprocess(file)
+        #self.receipts_repository.download_receipts()
+        #files = self.files_repository.list_input_files()
+        file = '20250623_081058 (Custom).jpg'
+        self.ocr_service.process_image(file)
         
         
         
